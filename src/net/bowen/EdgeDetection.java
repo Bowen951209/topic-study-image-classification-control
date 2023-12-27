26012581fe1d4cb78a635119172f3b052287e170
bowen951209 <bowen951209@gmail.com>
2023-12-27 08:52:57 +0800

    Rewrite and wrap code.

--- src/net/bowen/EdgeDetection.java
+++ src/net/bowen/EdgeDetection.java
@@ -14,28 +14,11 @@ public class EdgeDetection extends JFrame {
     /*
      * Reference: https://chtseng.wordpress.com/2016/12/05/opencv-edge-detection%E9%82%8A%E7%B7%A3%E5%81%B5%E6%B8%AC/
      * */
-    public EdgeDetection() {
-        // Prepare fot image
-        Mat src = Imgcodecs.imread("resources/pictures/houmai.jpg");
-        Imgproc.resize(src, src, new Size(), .8, .8);
-
-        // Turn to gray scale
-        Mat grayScale = new Mat();
-        Imgproc.cvtColor(src, grayScale, Imgproc.COLOR_BGR2GRAY);
-        src.release();
-
-        // Apply Laplacian edge detection
-        Mat finalImg = new Mat();
-        Imgproc.Laplacian(grayScale, finalImg, CvType.CV_16S, 3, 1, 0, Core.BORDER_DEFAULT);
-        grayScale.release();
-
-        // converting back to CV_8U
-        Core.convertScaleAbs(finalImg, finalImg);
-
-        // Convert img to bufferedImage
+    public EdgeDetection(Mat imgMat, String title) {
+        // Convert imgMat to bufferedImage
         MatOfByte matOfByte = new MatOfByte();
-        Imgcodecs.imencode(".jpg", finalImg, matOfByte);
-        finalImg.release();
+        Imgcodecs.imencode(".jpg", imgMat, matOfByte);
+        imgMat.release();
 
         BufferedImage bufferedImage;
         try {
@@ -49,16 +32,39 @@ public class EdgeDetection extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setSize(bufferedImage.getWidth(), bufferedImage.getHeight());
-        setTitle("Edge Detection");
+        setTitle(title);
         add(new JLabel(new ImageIcon(bufferedImage)));
         setVisible(true);
+    }
+
+    public static Mat laplacianEdgeDetection(Mat src) {
+        // Prepare fot image
+        Imgproc.resize(src, src, new Size(), .8, .8);
+
+        // Turn to gray scale
+        Mat grayScale = new Mat();
+        Imgproc.cvtColor(src, grayScale, Imgproc.COLOR_BGR2GRAY);
+        src.release();
+
+        // Apply Laplacian edge detection
+        Mat finalImg = new Mat();
+        Imgproc.Laplacian(grayScale, finalImg, CvType.CV_16S, 3, 1, 0, Core.BORDER_DEFAULT);
+        grayScale.release();
+
+        // Converting back to CV_8U
+        Core.convertScaleAbs(finalImg, finalImg);
+
+        return finalImg;
     }
 
     public static void main(String[] args) {
         // Init
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat src = Imgcodecs.imread("resources/pictures/houmai.jpg");
 
-        // Show on window.
-        new EdgeDetection();
+        // Get laplacian.
+        Mat laplacianResult = laplacianEdgeDetection(src);
+        // Create new window to display img.
+        new EdgeDetection(laplacianResult, "Laplacian Edge Detection");
     }
 }
