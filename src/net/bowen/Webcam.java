0de88cb3c5688c335eb251cb87840a30488d04aa
bowen951209 <bowen951209@gmail.com>
2024-02-25 16:55:33 +0800

    Receive data and print them to strings for COM3 at baud rate 115200 in Webcam class.

--- src/net/bowen/Webcam.java
+++ src/net/bowen/Webcam.java
@@ -1,5 +1,6 @@
 package net.bowen;
 
+import com.fazecast.jSerialComm.SerialPort;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
@@ -13,19 +14,42 @@ public class Webcam {
         Mat displayImg = new Mat();
         videoCapture.read(camImg);
         ObjectClassifier.detect(camImg, displayImg);
+        ImageMatDisplay display = new ImageMatDisplay(displayImg, "Webcam");
 
-        ImageMatDisplay display = new ImageMatDisplay(displayImg, "webcam");
+
+        System.out.println("Available ports:");
+        SerialPort[] availablePorts = SerialPort.getCommPorts();
+        System.out.println("-----------");
+        for (SerialPort availablePort : availablePorts) {
+            System.out.println(availablePort);
+        }
+        System.out.println("-----------");
+
+        SerialPort commPort = SerialPort.getCommPort("COM3");
+        commPort.setComPortParameters(115200, Byte.SIZE, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
+        commPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0);
+        if (!commPort.openPort())
+            System.err.println(commPort + " not available.");
+        else
+            System.out.println(commPort + " opened.");
+
         while (display.isDisplayable()) {
             videoCapture.read(camImg);
             ObjectClassifier.detect(camImg, displayImg);
-
             display.refresh();
+
+
+            if (commPort.bytesAvailable() > 0) {
+                byte[] readBuffer = new byte[commPort.bytesAvailable()];
+                commPort.readBytes(readBuffer, readBuffer.length);
+
+                System.out.println(new String(readBuffer));
+            }
         }
-
-
 
         videoCapture.release();
         camImg.release();
         displayImg.release();
+        commPort.closePort();
     }
 }
