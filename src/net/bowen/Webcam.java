2a56df74b6abf9fdaad6484cabb14cb1b27a1953
bowen951209 <bowen951209@gmail.com>
2024-02-25 17:26:49 +0800

    1. Auto open the port connecting to Arduino Uno. 2. Extract some code to methods and add comments.

--- src/net/bowen/Webcam.java
+++ src/net/bowen/Webcam.java
@@ -9,14 +9,46 @@ public class Webcam {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
+        // Init the camera image & the detection display image.
         VideoCapture videoCapture = new VideoCapture(0);
         Mat camImg = new Mat();
         Mat displayImg = new Mat();
         videoCapture.read(camImg);
         ObjectClassifier.detect(camImg, displayImg);
+
+        // The display window.
         ImageMatDisplay display = new ImageMatDisplay(displayImg, "Webcam");
 
+        // Get the port.
+        printAvailablePorts();
+        SerialPort commPort = getUNOPort();
 
+        while (display.isDisplayable()) {
+            // Update camImg and detect objects.
+            videoCapture.read(camImg);
+            ObjectClassifier.detect(camImg, displayImg);
+            display.refresh();
+
+
+            // Read data from the port and print it as a string.
+            if (commPort != null) {
+                if (commPort.bytesAvailable() > 0) {
+                    byte[] readBuffer = new byte[commPort.bytesAvailable()];
+                    commPort.readBytes(readBuffer, readBuffer.length);
+
+                    System.out.println(new String(readBuffer));
+                }
+            }
+        }
+
+        videoCapture.release();
+        camImg.release();
+        displayImg.release();
+        if (commPort != null)
+            commPort.closePort();
+    }
+
+    private static void printAvailablePorts() {
         System.out.println("Available ports:");
         SerialPort[] availablePorts = SerialPort.getCommPorts();
         System.out.println("-----------");
@@ -24,32 +56,36 @@ public class Webcam {
             System.out.println(availablePort);
         }
         System.out.println("-----------");
+    }
 
-        SerialPort commPort = SerialPort.getCommPort("COM3");
-        commPort.setComPortParameters(115200, Byte.SIZE, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
-        commPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0);
-        if (!commPort.openPort())
-            System.err.println(commPort + " not available.");
-        else
-            System.out.println(commPort + " opened.");
+    /**
+     * @return the port that connects to the Arduino UNO. If there's no port connects to it, return null.
+     */
+    private static SerialPort getUNOPort() {
+        SerialPort unoPort = null;
+        SerialPort[] availablePorts = SerialPort.getCommPorts();
 
-        while (display.isDisplayable()) {
-            videoCapture.read(camImg);
-            ObjectClassifier.detect(camImg, displayImg);
-            display.refresh();
-
-
-            if (commPort.bytesAvailable() > 0) {
-                byte[] readBuffer = new byte[commPort.bytesAvailable()];
-                commPort.readBytes(readBuffer, readBuffer.length);
-
-                System.out.println(new String(readBuffer));
+        // Get the port that is connected to Arduino UNO.
+        for (SerialPort port : availablePorts) {
+            if (port.getPortDescription().contains("Arduino Uno")) {
+                unoPort = port;
+                break;
             }
         }
 
-        videoCapture.release();
-        camImg.release();
-        displayImg.release();
-        commPort.closePort();
+        if (unoPort == null) {
+            System.err.println("Can't find a port connected to Arduino UNO");
+            return null;
+        }
+
+        unoPort.setComPortParameters(115200, Byte.SIZE, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
+        unoPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0);
+
+        if (!unoPort.openPort())
+            System.err.println(unoPort + " not available.");
+        else
+            System.out.println(unoPort + " opened.");
+
+        return unoPort;
     }
 }
